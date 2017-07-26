db149d94575633aa4febae057b1490c6c3cb3567
jmapzhouqing <jmapzhouqing@aliyun.com>
2017-07-26 08:15:23 +0800

    update

---
+++ Chapter_2/DrawRectangle.js
@@ -0,0 +1,13 @@
+function main(){
+	var canvas = document.getElementById("example");
+	
+	if(!canvas){
+		return false;
+	}
+	
+	var ctx = canvas.getContext("2d");
+	ctx.fillStyle = 'rgba(0,0,255,1.0)';
+	ctx.fillRect(120,10,150,150);
+
+	
+}
