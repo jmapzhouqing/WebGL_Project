03b19b221a689974475497e41e4aae71dbd10113
jmapzhouqing@aliyun.com <jmapzhouqing@aliyun.com>
2017-07-28 17:58:35 +0800

    'update'

---
+++ WebGL/content/name.js
@@ -0,0 +1,60 @@
+{
+
+	"metadata" :
+	{
+		"formatVersion" : 3.1,
+		"generatedBy"   : "Blender 2.7 Exporter",
+		"vertices"      : 8,
+		"faces"         : 6,
+		"normals"       : 8,
+		"colors"        : 0,
+		"uvs"           : [],
+		"materials"     : 1,
+		"morphTargets"  : 0,
+		"bones"         : 0
+	},
+
+	"scale" : 1.000000,
+
+	"materials" : [	{
+		"DbgColor" : 15658734,
+		"DbgIndex" : 0,
+		"DbgName" : "mat",
+		"blending" : "NormalBlending",
+		"colorAmbient" : [0.6400000190734865, 0.6400000190734865, 0.6400000190734865],
+		"colorDiffuse" : [0.6400000190734865, 0.6400000190734865, 0.6400000190734865],
+		"colorEmissive" : [0.0, 0.0, 0.0],
+		"colorSpecular" : [0.5, 0.5, 0.5],
+		"depthTest" : true,
+		"depthWrite" : true,
+		"mapDiffuse" : "cap.jpg",
+		"mapDiffuseWrap" : ["repeat", "repeat"],
+		"shading" : "Lambert",
+		"specularCoef" : 50,
+		"transparency" : 1.0,
+		"transparent" : false,
+		"vertexColors" : false
+	}],
+
+	"vertices" : [-1,-1,1,-1,1,1,-1,-1,-1,-1,1,-1,1,-1,1,1,1,1,1,-1,-1,1,1,-1],
+
+	"morphTargets" : [],
+
+	"normals" : [-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,-0.577349,0.577349,-0.577349,-0.577349,0.577349,0.577349,0.577349,0.577349,-0.577349,0.577349],
+
+	"colors" : [],
+
+	"uvs" : [],
+
+	"faces" : [35,2,0,1,3,0,0,1,2,3,35,3,7,6,2,0,3,4,5,0,35,7,5,4,6,0,4,6,7,5,35,0,4,5,1,0,1,7,6,2,35,0,2,6,4,0,1,0,5,7,35,5,7,3,1,0,6,4,3,2],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+  "animations" : []
+
+
+}
--- WebGL/js/WebGL.js
+++ WebGL/js/WebGL.js
@@ -3,7 +3,9 @@
  */
 
 var mesh;
+var part;
 var scene;
+var parent;
 var camera;
 var webGLRenderer
 var orbit;
@@ -11,6 +13,8 @@ var orbit;
 var gui;
 var mat;
 var controls;
+
+var helper;
 
 function create_ui(){
     controls = new function () {
@@ -31,11 +35,22 @@ function create_ui(){
             load_content(e);
         };
 
+        this.selectPart = function (e) {
+            load_part(e);
+        };
+
         this.changeTexture = function (e) {
             var texture = THREE.ImageUtils.loadTexture("../textures/" + e + ".jpg");
             mat.map = texture;
             if(mesh){
                 mesh.material.map = texture;
+            }
+        }
+
+        this.changeColor = function (e) {
+           var color = new THREE.Color(e);
+            if(mesh){
+                mesh.material.color = color;
             }
         }
     };
@@ -60,6 +75,10 @@ function create_ui(){
 
     spGui.add(controls, 'selectedMesh', ["cap", "sphere", "plane"]).onChange(controls.selectedMesh);
 
+    spGui.add(controls, 'selectPart', ["part", "sphere", "plane"]).onChange(controls.selectPart);
+
+    spGui.add(controls, 'changeColor', ["#FF0000", "#00FF00", "double"]).onChange(controls.changeColor);
+
     spGui.add(controls, 'changeTexture', ["cap", "daocao", "double"]).onChange(controls.changeTexture);
 }
 
@@ -68,6 +87,10 @@ function create_scene(){
     camera = new THREE.PerspectiveCamera(45, $("#WebGL-Output").width() /  $("#WebGL-Output").height(), 0.1, 1000);
     //orbit = new THREE.OrbitControls(camera);
 
+    helper = new THREE.AxisHelper(20);
+    scene.add(helper);
+    helper.rotation.y = 10;
+
     webGLRenderer = new THREE.WebGLRenderer({
         antialias: true
     });
@@ -75,43 +98,193 @@ function create_scene(){
     webGLRenderer.setSize($("#WebGL-Output").width(), $("#WebGL-Output").height());
     webGLRenderer.shadowMapEnabled = true;
 
-
-    camera.position.x = 130;
+    camera.position.x = -30;
     camera.position.y = 40;
     camera.position.z = 50;
     camera.lookAt(scene.position);
     scene.add(camera);
 
+
     var ambient_light =  new THREE.AmbientLight(0xffffff);
-    ambient_light.intensity = 0;
+    ambient_light.intensity = 0.5;
     scene.add(ambient_light);
+
 
     var direct_light = new THREE.DirectionalLight(0xcccccc);
     direct_light.position.set(30, 40, 50);
-    direct_light.intensity = 1;
+    direct_light.intensity = 0.2;
     scene.add(direct_light);
+    parent = new THREE.Object3D();
+    scene.add(parent);
+
+    parent.rotation.set(0,0,0);
 
     $("#WebGL-Output").append(webGLRenderer.domElement);
 
     render();
+
+    load_content("sphere");
+
+    Listener();
 }
 
 function load_content(name){
-    controls.changeTexture(name);
-    var loader = new THREE.JSONLoader();
+    /*var loader = new THREE.JSONLoader();
     loader.load('../content/'+name+'.js', function (geometry,material) {
-        mesh = new THREE.Mesh(geometry,mat);
-        mesh.position.set(0,0,0);
-        mesh.scale.set(500, 500, 500);
-        scene.add(mesh);
-    },"../textures");
+        if(mesh){
+            scene.remove(mesh);
+        }
+        mat = material[0];
+        mesh = new THREE.Mesh(geometry, mat);
+        mesh.scale.x = 15;
+        mesh.scale.y = 15;
+        mesh.scale.z = 15;
+        //container.position.set(0,0,0);
+        //mesh.scale.set(500, 500, 500);
+        parent.add(mesh);
+    },"../textures/");*/
+
+    //controls.changeTexture(name);
+
+    var loader = new THREE.OBJMTLLoader();
+    loader.load('../content/buffterfly.obj','../content/buffterfly.mtl',function(loadedMesh){
+        mesh = loadedMesh;
+        loadedMesh.scale.set(100, 100, 100);
+        loadedMesh.position.set(0,0,0);
+        scene.add(loadedMesh);
+    });
+}
+
+function load_part(name){
+    var loader = new THREE.JSONLoader();
+    loader.load('../content/part.js', function (geometry,material){
+        //container.position.set(0,0,0);
+        //mesh.scale.set(500, 500, 500);
+        if(mesh){
+            if(part){
+                mesh.remove(part);
+            }
+            mat = material[0];
+            part = new THREE.Mesh(geometry, mat);
+            mesh.add(part);
+            part.scale.x = 1;
+            part.scale.y = 1;
+            part.scale.z = 1;
+            part.position.set(0,0,0);
+        }
+        //scene.add(mesh);
+    },"../textures/");
 }
 
 function render() {
     //orbit.update();
     if (mesh) {
+        //mesh.rotation.y += 0.02;
 
     }
     requestAnimationFrame(render);
     webGLRenderer.render(scene, camera);
 }
+
+function Listener(){
+    var startX,endX,startY,endY;
+    var _down = false;
+    $("#WebGL-Output").on('mousemove mousedown mouseup',function(event){
+        /*
+        var touch_first = event.originalEvent.targetTouches[0],
+            touch_second = event.originalEvent.targetTouches[1],
+            fingers = event.originalEvent.touches.length;
+        if (event.type == 'touchstart') {
+            startX = touch_first.pageX;
+            startY = touch_first.pageY;
+
+        } else if (event.type == 'touchmove') {
+            endX = Math.abs(touch_first.pageX - startX);
+            endY = Math.abs(touch_first.pageY - startY);
+            mesh.rota
+            mesh.rotation.x += endY * 0.01;
+        } else if (event.type == 'touchend') {
+
+        }*/
+
+        if (event.type == 'mousedown') {
+            startX = event.clientX;
+            startY = event.clientY;
+            _down = true;
+        } else if (event.type == 'mousemove') {
+            if(!_down){
+                return;
+            }
+            endX = event.clientX - startX;
+            endY = event.clientY - startY;
+
+            startX = event.clientX;
+            startY = event.clientY;
+            //mesh.rota
+            //rotateAroundWorldAxis(parent,new THREE.Vector3(1,0,0),endY * 0.01);
+            //rotateAroundWorldAxis(parent,camera.up,endX * 0.01);
+            var vec = new THREE.Vector3(0,0,0);
+            //vec.crossVectors(camera.up,camera.getWorldDirection())
+            rotateAroundWorldAxis(parent,new THREE.Vector3(1,0,0),endX * 0.01);
+            //mesh.rotateY(endX*0.01);
+        }else if(event.type == 'mouseup'){
+            _down = false;
+        }
+    });
+}
+
+function makeRotationAxis( axis, angle ) {
+    var c = Math.cos( angle );
+    var s = Math.sin( angle );
+    var t = 1 - c;
+    var x = axis.x, y = axis.y, z = axis.z;
+    var tx = t * x, ty = t * y;
+    this.set(
+        tx * x + c, tx * y - s * z, tx * z + s * y, 0,
+        tx * y + s * z, ty * y + c, ty * z - s * x, 0,
+        tx * z - s * y, ty * z + s * x, t * z * z + c, 0,
+        0, 0, 0, 1
+    );
+    return this;
+}
+
+var rotWorldMatrix;
+
+function rotateAroundWorldAxis(object, axis, radians) {
+
+    rotWorldMatrix = new THREE.Matrix4();
+
+    rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
+
+    rotWorldMatrix.multiply(object.matrix);                // pre-multiply
+
+    object.matrix = rotWorldMatrix;
+
+    object.rotation.setFromRotationMatrix(object.matrix);
+
+}
+
+/*
+var touchScale = function(seletor, bg) {
+        var startX, endX, scale, x1, x2, y1, y2, imgLeft, imgTop, imgWidth, imgHeight,
+        one = false,
+        $touch = $(seletor),
+        originalWidth = $touch.width(),
+        originalHeight = $touch.height(),
+        baseScale = parseFloat(originalWidth/originalHeight),
+        imgData = [],
+        bgTop = parseInt($(bg).css('top'));
+    function siteData(name) {
+        imgLeft = parseInt(name.css('left'));
+        imgTop = parseInt(name.css('top'));
+        imgWidth = name.width();
+        imgHeight = name.height();
+    }
+
+    var getData = function(){
+        return imgData;
+    };
+    return {
+        imgData : getData
+    }
+};*/
